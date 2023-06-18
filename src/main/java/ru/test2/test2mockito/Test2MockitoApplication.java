69fb7945fb8da8c9f8965bd66728875f7fe64fa5
rom064 <rom064@mail.ru>
2023-06-18 22:57:13 +0500

    ДЗ Тестирование часть 2 Mockito

---
+++ src/main/java/ru/test2/test2mockito/Test2MockitoApplication.java
@@ -0,0 +1,13 @@
+package ru.test2.test2mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2MockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test2MockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/test2/test2mockito/Test2MockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.test2.test2mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test2MockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
